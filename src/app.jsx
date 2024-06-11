eb55983a886ccaf7c08491eb8ee9da3143f4db0f
John Herr <jhchnc@gmail.com>
2024-06-10 21:09:04 -0400

    add yaml

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,14 @@
 import './styles/app.scss'
+import YAML from 'yaml'
+
+async function GetToc() {
+	const res = await fetch('./thresholds.yml')
+	const thresholds = await res
+	console.log(thresholds.text())	
+}
 
 const Foo = (props) => {
+	GetToc()
 	return (
 		<strong>{props.words}</strong>
 	)
